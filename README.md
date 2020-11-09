60769abe5ec6ae483aa00a42753103f41451061d
Naranbataar <44126803+Naranbataar@users.noreply.github.com>
2020-11-08 22:26:57 -0300

    Improvements and reorganization

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Build
+
+Example usage:
+```sh
+. scripts/config.sh
+
+# If you need crosstools
+scripts/meta/tools
+export PATH="$PATH:$TOOLS/bin"
+
+# Base packages
+scripts/base/musl-*
+scripts/base/busybox-*
+scripts/base/linux-*
+scripts/base/uboot-*
+
+# Formating the SD card
+printf 'o\n n\n p\n 1\n 2048\n \n a\n w\n' | fdisk /dev/mmcblk0
+mkfs.ext4 "/dev/mmcblk0p1"
+
+# Installing the packages
+mkdir -p .mnt .info
+mount "/dev/mmcblk0p1" .mnt
+
+chateau -r .mnt -i .info add "$BUILD/filesystem.tar.xz"
+chateau -r .mnt -i .info add "$BUILD/network.tar.xz"
+chateau -r .mnt -i .info add "$BUILD/linux-"*.tar.xz
+chateau -r .mnt -i .info add "$BUILD/musl-"*.tar.xz
+chateau -r .mnt -i .info add "$BUILD/busybox-"*.tar.xz
+
+cp scripts/chateau .mnt/bin/
+mv .info .mnt/etc/packages
+
+umount .mnt && rm -rf .mnt
+
+# Flashing u-boot
+dd if="$BUILD/u-boot.bin" of=/dev/mmcblk0 bs=1024 seek=8
+```
+
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,35 +1,39 @@
 #!/bin/sh
+. scripts/config.sh
 
 not_built(){
     [ ! -f "$BUILD/$PKG.tar.xz" ]
 }
 
 inside(){
-    url="$1"
-    file="${url##*/}"; dir="${file%.tar*}"
+    url="$1"; file="${url##*/}"; dir="${file%.tar*}"
     
     cd "$DOWNLOADS"
     if [ ! -d "$dir" ]; then
-        rm -f "$ROOT/.step"
+        rm -f "$ROOT/.step-$PKG"
         if curl -L "$url" > "$file" && tar -xf "$file"; then
             cd "$dir"
         else
-            exit 1
+            return 1
         fi
     else
         cd "$dir"
     fi
+
+    unset url file dir
 }
 
 step(){
-    [ "$(cat "$ROOT/.step" 2>/dev/null || printf '1\n')" -gt "$1" ]
+    [ "$(cat "$ROOT/.step-$PKG" 2>/dev/null || printf '1\n')" -gt "$1" ]
     ret="$?"
 
-    printf '%d\n' "$(( $1 + 1 ))" > "$ROOT/.step" || rm -f "$ROOT/.step"
+    printf '%d\n' "$(( $1 + 1 ))" > "$ROOT/.step-$PKG" \
+    || rm -f "$ROOT/.step-$PKG"
+
     return "$ret"
 }
 
 build(){
     cd "$BUILD/$1" && chateau make "$BUILD/$1.tar.xz" && rm -rf "$BUILD/$1" \
-    && rm -f "$ROOT/.step"
+    && rm -f "$ROOT/.step-$PKG"
 }
